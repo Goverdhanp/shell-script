4c664b3fd8dfaeb58197d8d7d47ec602df863859
Goverdhan <137044283+Goverdhanp@users.noreply.github.com>
2025-04-15 16:35:47 +0530

    new col

--- echo.sh
+++ echo.sh
@@ -9,9 +9,9 @@ echo hello world
 
 # col are 31-red,32-green,33-yellow,34-blue,35-magenta,36-cyan
 
-echo -e"\e[31mHello World in red color\e[0m"
-echo -e"\e[32mHello World in green color\e[0m"
-echo -e"\e[33mHello World in yellow color\e[0m"
-echo -e"\e[34mHello World in blue color\e[0m"
-echo -e"\e[35mHello World in magenta color\e[0m"
-echo -e"\e[36mHello World in cyan color\e[0m"
+echo -e "\e[31mHello World in red color\e[0m"
+echo -e "\e[32mHello World in green color\e[0m"
+echo -e "\e[33mHello World in yellow color\e[0m"
+echo -e "\e[34mHello World in blue color\e[0m"
+echo -e "\e[35mHello World in magenta color\e[0m"
+echo -e "\e[36mHello World in cyan color\e[0m"

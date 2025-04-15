489e3189338da919b2e61caf97d55e9797ecc60a
Goverdhan <137044283+Goverdhanp@users.noreply.github.com>
2025-04-15 17:13:25 +0530

    var

--- 01-variable.sh
+++ 01-variable.sh
@@ -6,3 +6,31 @@ a=10
 
 #echo will print the data you provide
 echo $a
+
+# Variable are functions naming 
+#we can use characters a-z,A-Z
+#we can use numbers and underscore
+
+#input variables
+
+
+#                     <input variable>
+
+#   <input variable>        script      <input variable>
+
+
+#                       env variables
+# LHS       script    RHS
+# environment variables
+# export var=data
+
+#on command line run x1=100 before executing this script
+echo x1-$x1
+
+#1.lhs
+#x2=200 script
+echo x2-$x2
+
+#1.rhs
+# script x2=200
+echo first argument -$1
